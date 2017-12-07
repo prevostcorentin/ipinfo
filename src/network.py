5ef704770ec901dfc5c830a49a37d1835382c1d8
SYSINF\tsdi202 <cocauw@gmail.com>
2017-12-07 10:57:12 +0100

    not mask but cidr

--- src/network.py
+++ src/network.py
@@ -4,10 +4,11 @@ from math import pow
 
 class Network(object):
 
-    def __init__(self, ip, mask):
+    def __init__(self, ip, cidr):
         self._ip = ip
-        self._mask = int(mask)
-        self._subnetwork_bits = int(mask) - ip.mask
+        self._mask = ''
+        self._cidr = int(cidr)
+        self._subnetwork_bits = int(cidr) - ip.mask
 
     @property
     def subnetworks(self):
@@ -15,22 +16,24 @@ class Network(object):
 
     @property
     def machines(self):
-        return int(pow(2, 32 - self._mask))
+        return int(pow(2, 32 - self._cidr))
 
     @property
     def mask(self):
-        addr = []
-        for x in xrange(self._mask / 8):
-            addr.append(255)
-        left_bytes = self._mask % 8
-        if left_bytes:
-            last_index = len(addr)
-            addr.append(0)
-            for x in xrange(1, left_bytes+1):
-                addr[last_index] += pow(2, 8 - x)
-        while len(addr) < 3:
-            addr.append(0)
-        return '.'.join([str(p) for p in addr])
+        if not self._mask:
+            addr = []
+            for x in xrange(self._cidr / 8):
+                addr.append(255)
+            left_bytes = self._cidr % 8
+            if left_bytes:
+                last_index = len(addr)
+                addr.append(0)
+                for x in xrange(1, left_bytes+1):
+                    addr[last_index] += pow(2, 8 - x)
+            while len(addr) < 3:
+                addr.append(0)
+            self._mask = '.'.join([str(p) for p in addr])
+        return self._mask
 
     @property
     def beginning_addr(self, subnetwork_nb):
